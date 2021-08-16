c524e313e6c44301d3ccc76d782ed3718f1af0f2
robson <robson1992ferreira@hotmail.com>
2021-08-16 15:57:18 -0300

    update readme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header/Header';
 import Banner from './Components/Banner/Banner';
